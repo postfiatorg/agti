907691ed3720460c0ababd232f6f2b79da648784
goodalexander <goodalexander@gmail.com>
2024-05-23 10:04:30 -0400

    added setup function
    Post Fiat finance node, initial functions

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='agti',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'sqlalchemy',
+        'cryptography',
+        'xrpl-py',
+        'wxPython',
+        'requests',
+        'toml',
+        'nest_asyncio',
+    ],
+    author='Alex Good',
+    author_email='alex@agti.net',
+    description='Post Fiat Finance focused node',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/postfiatorg/pftpyclient',  # Replace with your actual GitHub repo URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
