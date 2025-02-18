6ed1665a6bc7fdb399030298607a3f4df9b04015
Lukas Kiss <lukaskiss222@gmail.com>
2025-02-18 12:08:12 +0100

    add support for categories in base_scraper

--- agti/central_banks/base_scrapper.py
+++ agti/central_banks/base_scrapper.py
@@ -38,6 +38,9 @@ class BaseBankScraper:
         """Retrieve machine IP and hostname."""
         hostname = socket.gethostname()
         return socket.gethostbyname(hostname), hostname
+    
+    def get_category_table_name(self):
+        return f"{self.table_name}_categories"
 
     def get_all_db_urls(self):
         """Retrieve all URLs already stored in the database."""
@@ -46,13 +49,28 @@ class BaseBankScraper:
         params = {"country_code_alpha_3": self.COUNTRY_CODE_ALPHA_3}
         with dbconnx.connect() as con:
             rs = con.execute(query, params)
-            return [row[0] for row in rs.fetchall()]
+            output =  [row[0] for row in rs.fetchall()]
+        return output
+        
+    def get_all_db_categories(self):
+        """Retrieve all categories already stored in the database."""
+        dbconnx = self.db_connection_manager.spawn_sqlalchemy_db_connection_for_user(self.user_name)
+        # query select all from categories table, which joined with self.table_name over file_url has country_code_alpha_3
+        table_name = self.get_category_table_name()
+        query = text(f"SELECT {table_name}.file_url {table_name}.category_name FROM {table_name} " +  \
+                    "INNER JOIN {self.table_name} ON {table_name}.file_url = {self.table_name}.file_url " + \
+                    "WHERE {self.table_name}.country_code_alpha_3 = :country_code_alpha_3")
+        params = {"country_code_alpha_3": self.COUNTRY_CODE_ALPHA_3}
+        with dbconnx.connect() as con:
+            rs = con.execute(query, params)
+            output =  [row for row in rs.fetchall()]
+        return output
 
     def add_to_db(self, data):
         """Store scraped data into the database."""
         df = pd.DataFrame(data)
         if df.empty:
-            print("No new data found.")
+            logger.info("No new data found.")
             return
         
         ipaddr, hostname = self.ip_hostname()
@@ -93,3 +111,44 @@ class BaseBankScraper:
 
         dbconnx = self.db_connection_manager.spawn_sqlalchemy_db_connection_for_user(self.user_name)
         df.to_sql(self.table_name, con=dbconnx, if_exists="append", index=False)
+
+    def add_to_categories(self, data):
+        """Store scraped data into categories table."""
+        df = pd.DataFrame(data)
+        if df.empty:
+            logger.info("No new data found for categories.")
+            return
+        
+        # duplicated file_url and category_name
+        duplicated = df[["file_url", "category_name"]].duplicated()
+        # log each duplicated file_url with date published
+        for _, row in df[duplicated].iterrows():
+            url = row["file_url"]
+            category_name = row["category_name"]
+            logger.warning(f"Duplicate file_url and category_name found: {url} - {category_name}",
+                           extra={
+                                "url": url,
+                                "category_name": category_name
+                           })
+        # drop duplicates on file_url and category_name
+        df = df.drop_duplicates(subset=["file_url", "category_name"])
+
+        # verify unique over whole table
+        db_categories = self.get_all_db_categories()
+        # all should be new otherwise raise warning for each not new url
+        for _, row in df.iterrows():
+            url = row["file_url"]
+            category_name = row["category_name"]
+            if (url, category_name) in db_categories:
+                logger.warning(f"URL and category_name already in database: {url} - {category_name}", extra={
+                    "url": url,
+                    "category_name": category_name})
+        
+        # drop all urls in database on file_url
+        df = df[~df[["file_url", "category_name"]].isin(db_categories).all(axis=1)]
+
+
+
+        dbconnx = self.db_connection_manager.spawn_sqlalchemy_db_connection_for_user(self.user_name)
+        table_name = self.get_category_table_name()
+        df.to_sql(table_name, con=dbconnx, if_exists="append", index=False)
