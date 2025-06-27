088daa235440db3bdc16a9dac6f8bd7ad1eb45df
Lukas Kiss <lukaskiss222@gmail.com>
2025-06-27 12:57:56 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,9 @@ setup(
         'requests',
         'toml',
         'nest_asyncio','brotli','sec-cik-mapper','psycopg2-binary','quandl','schedule','openai','lxml',
-        'gspread_dataframe','gspread','oauth2client'
+        'gspread_dataframe','gspread','oauth2client',
+        'selenium',"selenium-wire>=5.1.0,<6",'boto3','blinker==1.7',
+        'ua_generator',
     ],
     author='Alex Good',
     author_email='alex@agti.net',
